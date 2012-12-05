ea8f12f098b18000fb1dbd0593d5f049e07ffdaf
Ryan Marquardt <ryan.marquardt@gmail.com>
2012-12-04 22:29:30 -0800

    WIP apache2.py: Configuration file parsing and editing

--- angler/apache2.py
+++ angler/apache2.py
@@ -2,6 +2,8 @@
 
 from angler.common import *
 
+from collections import OrderedDict as od
+
 logging = getLogger('apache2')
 
 sites_available_dir = Folder('/etc/apache2/sites-available')
@@ -12,4 +14,52 @@ class Server(Definition):
 		Package('apache2') > self
 		Service('apache2', state='running')
 		Definition.__init__(self)
+
+class Apache2File(Path):
+	def __init__(self, path):
+		Path.__init__(self, path, state='file')
+
+	@param
+	def content(self):
+		def parse_block(I, end=None):
+			for line in I:
+				line = line.partition('#')[0].strip()
+				if not line:
+					continue
+				if line.startswith('<'):
+					if end and line == '</%s>' % end:
+						return
+					else:
+						cmd,args = line[1:-1].split(None, 1)
+						yield (cmd,args), od(parse_block(I, cmd))
+				else:
+					cmd,args = line.split(None, 1)
+					yield cmd, args
+		return od(parse_block(iter(open(self.path,'rb'))))
+	@content.validator
+	def content(self, new):
+		if isinstance(new, dict):
+			return new
+		elif isinstance(new, SEQUENCE_TYPES):
+			return od(new)
+		raise InvalidParam
+
+	def runners(self):
+		print self.content()
+		raise Exception
+		yield
 		
+class Conf(Definition):
+	def __init__(self):
+		Definition.__init__(self)
+		self.ports_conf = Apache2File('/etc/apache2/ports.conf')
+		print self.ports_conf.content()
+
+	@param
+	def http_port(self):
+		return int(self.ports_conf['Listen'])
+	@http_port.validator
+	def http_port(self, new):
+		if isinstance(new, int) and 0 <= new <= 65535:
+			return new
+		raise InvalidParam
