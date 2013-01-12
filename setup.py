e9d0de2c7823e572bc3b0886482f21d5e360ffdc
Ryan Marquardt <ryan.marquardt@gmail.com>
2013-01-11 17:31:57 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='angler', version='0.0.1',
+	url='http://packages.integralws.com/angler',
+	author='Ryan Marquardt',
+	author_email='ryan@integralws.com',
+	packages=['angler', 'angler/etc', 'angler/package'],
+	scripts=[],
+)
