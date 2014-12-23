fd7a44c7a8b3c796ed3e6ad611205d3f85b0550a
Ryan Marquardt <ryan.marquardt@gmail.com>
2014-12-23 11:07:52 -0800

    Missed incorporating properties into nodes

--- test/commands.sh
+++ test/commands.sh
@@ -8,8 +8,8 @@ export PATH="bin:$PATH"
 debug angler-setup
 debug angler-add package:///bash install
 debug angler-add path:///home/user/bin/script file
-debug angler-add path:///home/user/bin/script?permission 0755
-debug angler-add process:///run-script "bash bin/script"
+debug angler-add path:///home/user/bin/script?permission exact mode=0755
+debug angler-add process:///run-script once command="bash bin/script"
 debug angler-add path:///home/user/.bashrc file --before process:///run-script
 debug angler-order package:///bash exec:///run-script
 debug angler-run -n
