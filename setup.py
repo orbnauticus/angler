6abaf4f50e6deded2cde2f6c804d810c99f61033
Ryan Marquardt <ryan.marquardt@gmail.com>
2014-12-22 16:51:04 -0800

    PEP8 fixes

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='angler', version='0.0.1',
-	url='http://packages.integralws.com/angler',
-	author='Ryan Marquardt',
-	author_email='ryan@integralws.com',
-	packages=['angler', 'angler/etc', 'angler/package'],
-	scripts=[],
+setup(
+    name='angler', version='0.0.1',
+    url='http://packages.integralws.com/angler',
+    author='Ryan Marquardt',
+    author_email='ryan@integralws.com',
+    packages=['angler', 'modules'],
+    scripts=[],
 )
