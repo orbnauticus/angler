b077d3a32ebdba0403875e425d3028213934f9b8
Ryan Marquardt <ryan.marquardt@gmail.com>
2014-12-26 13:15:19 -0800

    Jump directly into loop over args.command

--- angler/shell/__main__.py
+++ angler/shell/__main__.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('manifest', nargs='?', default=default_manifest)
 parser.add_argument('-n', '--dryrun', action='store_true')
-parser.add_argument('-c', '--command', action='append')
+parser.add_argument('-c', '--command', action='append', default=())
 parser.add_argument('-i', '--interactive', action='store_true')
 
 args = parser.parse_args()
@@ -25,9 +25,8 @@ try:
     shell = AnglerShell(manifest)
     shell.environment['dryrun'] = args.dryrun
 
-    if args.command:
-        for command in args.command:
-            shell.onecmd(command)
+    for command in args.command:
+        shell.onecmd(command)
     if not args.command or args.interactive:
         shell.cmdloop()
 except KeyboardInterrupt:
