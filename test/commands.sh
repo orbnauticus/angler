4e5ea83f85272098a374f31774b1eea0de62e385
Ryan Marquardt <ryan.marquardt@gmail.com>
2014-12-25 17:47:52 -0800

    Support reading commands from stdin

--- test/commands.sh
+++ test/commands.sh
@@ -3,15 +3,14 @@
 debug () { echo '**' "$@"; "$@"; }
 
 rm angler.manifest 2>/dev/null || true
-export PYTHONPATH=.
-export PATH="bin:$PATH"
-debug angler-setup
-debug angler-add package:///bash install
-debug angler-add path:///home/user/bin/script file
-debug angler-add path:///home/user/bin/script?permission exact mode=0755
-debug angler-add process:///run-script once command="bash bin/script"
-debug angler-add path:///home/user/.bashrc file --before process:///run-script
-debug angler-order package:///bash exec:///run-script
-debug angler-run -n
-debug angler-run -n --swap
+PYTHONPATH=. bin/angler-shell <<EOF
+add package:///bash install
+add path:///home/user/bin/script file
+add path:///home/user/bin/script?permission exact mode=0755
+add process:///run-script once command="bash bin/script"
+add path:///home/user/.bashrc file --before process:///run-script
+order package:///bash exec:///run-script
+run
+run --swap
+EOF
 rm angler.manifest
