c352a4192a288c7a5b99220b204146201a44a494
Ryan Marquardt <ryan.marquardt@gmail.com>
2014-12-26 13:12:27 -0800

    Created abstract Shell superclass for AnglerShell

--- angler/shell/__init__.py
+++ angler/shell/__init__.py
@@ -4,6 +4,8 @@ from angler.core import Manifest, default_manifest, Session, setup
 from angler.command import Add, Order, Apply, Setup
 from angler.util import uri, key_value
 
+from .shell import Shell
+
 import argparse
 import cmd
 import os
@@ -18,85 +20,15 @@ def single_arg(string):
     return values[0]
 
 
-class AnglerShell(cmd.Cmd):
+class AnglerShell(Shell):
     def __init__(self, manifest, history='~/.angler_history'):
-        super(AnglerShell, self).__init__()
-        self.isatty = sys.stdin.isatty()
-        self.environment = dict()
+        super(AnglerShell, self).__init__(
+            history,
+            prompt='{manifest}#{curdir}⟫')
         self.manifest = manifest
         self.session = Session(manifest)
         self.environment['manifest'] = manifest.database
         self.environment['curdir'] = ''
-        self.prompt = '{manifest}#{curdir}⟫'
-        self.environment['prompt2'] = '>'
-        self.multiline = ''
-        self.history = os.path.expanduser(history)
-        try:
-            import readline
-            readline.read_history_file(self.history)
-        except FileNotFoundError:
-            pass
-        except ImportError:
-            pass
-        else:
-            import atexit
-            atexit.register(readline.write_history_file, self.history)
-
-    @property
-    def prompt(self):
-        if not self.isatty:
-            return ''
-        elif self.multiline:
-            return self.environment['prompt2'] + ' '
-        else:
-            return self.environment['prompt'].format(**self.environment) + ' '
-
-    @prompt.setter
-    def prompt(self, new):
-        self.environment['prompt'] = new
-
-    def emptyline(self):
-        pass
-
-    def parseline(self, line):
-        if self.multiline:
-            line = '{}\n{}'.format(self.multiline, line)
-        self.multiline = ''
-        try:
-            words = shlex.split(line)
-        except ValueError as error:
-            if error.args == ('No closing quotation',):
-                self.multiline = line
-                return None, None, ''
-            elif error.args == ('No escaped character',):
-                self.multiline = line[:-1]
-                return None, None, ''
-            else:
-                raise
-        if not words:
-            return None, None, ''
-        cmd, arg = words[0], words[1:]
-        return cmd, arg, line
-
-    def default(self, line):
-        if line == 'EOF':
-            if self.isatty:
-                print('exit')
-            return self.do_exit('')
-        else:
-            return super(AnglerShell, self).default(line)
-
-    def do_exit(self, args):
-        try:
-            import readline
-            last_index = readline.get_current_history_length()
-            last_command = readline.get_history_item(last_index)
-            if (last_command is not None and (last_command == 'exit'
-                    or last_command.startswith('exit '))):
-                readline.remove_history_item(last_index-1)
-        except ImportError:
-            pass
-        return True
 
     def do_help(self, args):
         super(AnglerShell, self).do_help(args[0])
---
+++ angler/shell/shell.py
@@ -0,0 +1,83 @@
+
+import cmd
+import os
+import shlex
+import sys
+
+
+class Shell(cmd.Cmd):
+    def __init__(self, history, prompt='$'):
+        super(Shell, self).__init__()
+        self.isatty = sys.stdin.isatty()
+        self.environment = dict()
+        self.environment['prompt'] = prompt
+        self.environment['prompt2'] = '>'
+        self.multiline = ''
+        if history is not None:
+            self.history = os.path.expanduser(history)
+            try:
+                import readline
+                readline.read_history_file(self.history)
+            except FileNotFoundError:
+                pass
+            except ImportError:
+                pass
+            else:
+                import atexit
+                atexit.register(readline.write_history_file, self.history)
+
+    @property
+    def prompt(self):
+        if not self.isatty:
+            return ''
+        elif self.multiline:
+            return self.environment['prompt2'] + ' '
+        else:
+            return self.environment['prompt'].format(**self.environment) + ' '
+
+    @prompt.setter
+    def prompt(self, new):
+        self.environment['prompt'] = new
+
+    def emptyline(self):
+        pass
+
+    def parseline(self, line):
+        if self.multiline:
+            line = '{}\n{}'.format(self.multiline, line)
+        self.multiline = ''
+        try:
+            words = shlex.split(line)
+        except ValueError as error:
+            if error.args == ('No closing quotation',):
+                self.multiline = line
+                return None, None, ''
+            elif error.args == ('No escaped character',):
+                self.multiline = line[:-1]
+                return None, None, ''
+            else:
+                raise
+        if not words:
+            return None, None, ''
+        cmd, arg = words[0], words[1:]
+        return cmd, arg, line
+
+    def default(self, line):
+        if line == 'EOF':
+            if self.isatty:
+                print('exit')
+            return self.do_exit('')
+        else:
+            return super(Shell, self).default(line)
+
+    def do_exit(self, args):
+        try:
+            import readline
+            last_index = readline.get_current_history_length()
+            last_command = readline.get_history_item(last_index)
+            if (last_command is not None and (last_command == 'exit'
+                    or last_command.startswith('exit '))):
+                readline.remove_history_item(last_index-1)
+        except ImportError:
+            pass
+        return True
