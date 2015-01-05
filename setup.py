740e67246301727e52e4cd18f523011d5d2af876
Ryan Marquardt <ryan.marquardt@gmail.com>
2015-01-04 16:10:04 -0800

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='angler', version='0.0.1',
+    name='angler', version='0.2',
     url='http://packages.integralws.com/angler',
     author='Ryan Marquardt',
     author_email='ryan@integralws.com',
-    packages=['angler', 'modules'],
-    scripts=[],
+    packages=['angler'],
+    package_data={
+        'angler': ['modules/*.*'],
+    },
+    scripts=['bin/angler-shell'],
 )
